63028a35afa4f28ce4de8a7d65ab6c8c59eb2ec0
gerrykou <gerasimoskounadis@gmail.com>
2021-11-26 19:07:23 +0000

    add comment

--- social_media_data_parser.py
+++ social_media_data_parser.py
@@ -6,7 +6,7 @@
 
 # The scraper should
 #     1. not return out of range dates 
-#     2. return dates with specific format - "%d/%m/%y"
+#     2. return dates with consistent specific format - "%d/%m/%y"
 #     3. validated structure lists
 
 import json
@@ -21,7 +21,7 @@ json_string = '''{
     "periodStart" : "15/02/11",
     "periodEnd" : "31/08/21",
     "monthlyPostingDay" : 11,
-    "comments" :[["2/3/21","Justin Bieber",5], ["5/4/21", "Lady Gaga", 6],["5/4/21", "Snoop Dog", 2], ["13/5/21","Justin Bieber", 3] ]
+    "comments" :[["2/3/21","Justin Bieber",5], ["5/4/21", "Lady Gaga", 6],["5/4/21", "Snoop Dog", 2], ["13/5/21","Justin Bieber", 3]]
 }'''
 
 
