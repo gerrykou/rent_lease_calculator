7cc52df2043dffa8935f31c3ff69cd69a79236fa
gerrykou <gerasimoskounadis@gmail.com>
2021-11-27 03:51:12 +0000

    calculate dates and rent review

--- rent_lease.py
+++ rent_lease.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
-import datetime
+import datetime#
+
 #2/ Can you write another python script that takes the following input data as arguments on the
 # command line:
 # (i) a lease start date
@@ -13,7 +14,11 @@ import datetime
 # a) the rent review dates that would apply during the lease
 # b) calculate the rent review amount for each rent review if we assume that the rent grows by
 # n% at each review
+
 FORMAT = "%Y-%m-%d"
+N = 0.11
+RENT_INCREASE = 1 + N
+RENT_ROUND_DECIMAL = 0
 
 
 def parse_arguments():
@@ -46,13 +51,17 @@ def parse_arguments():
 
 
 def _date_str2obj(date_string, format):
-    #returns date object from string 
+
     try:
         date_obj = datetime.datetime.strptime(date_string, format)
     except:
-        print(f"ERROR: Date {date_string} is not in a correct date format or not a valid date. It should be in ISO 8601 format YYYY-MM-DD, try --help for more info")
-        exit(10) 
+        raise argparse.ArgumentTypeError(f"Date {date_string} is not in a correct date format or not a valid date. It should be in ISO 8601 format YYYY-MM-DD, try --help for more info") 
     return date_obj.date()
+
+def _datetime_obj2string(input_date, format):
+    date_obj=datetime.datetime.strftime(input_date, format)
+    return date_obj
+
 
 def _validate_end_date_greater_than_start_date(start_date, end_date):
     if start_date > end_date :
@@ -67,28 +76,49 @@ def check_positive_integer(input_number):
         raise argparse.ArgumentTypeError(f'Must be a positive integer number, not {input_number}')
     return input_number
 
+def calculate_rent_review_dates(start_date_obj, end_date_obj, rev_freq):
+    output =[]
+    for date in _iter_dates(start_date_obj, end_date_obj, rev_freq):
+        output.append(_datetime_obj2string(date, FORMAT))
+    return output
+
+def _iter_dates(start_date_obj, end_date_obj, rev_freq):
+    next_date = start_date_obj
+    while next_date < end_date_obj :
+        new_date_obj=datetime.date(next_date.year + rev_freq, next_date.month, next_date.day)
+        yield next_date
+        next_date = new_date_obj
+
+def calculate_increasing_rent(RENT, review_dates_list):
+    rent_increase_dict = {}
+    increasing_rent = RENT
+    for i in review_dates_list:
+        rent_increase_dict[i] = round(increasing_rent * RENT_INCREASE , RENT_ROUND_DECIMAL)
+        increasing_rent = rent_increase_dict[i]
+    return rent_increase_dict
 
 
 def main():
-    while True:
-        try:
-            args = parse_arguments()
+    try:
+        args = parse_arguments()
 
-            print("Program arguments:", args)
-            
-            START_DATE = args.start_date
-            END_DATE = args.end_date
-            FIRST_REVIEW_DATE = args.first_review_date
-            REVIEW_FREQ = args.review_freq
-            RENT = args.rent
+        print("Program arguments:", args)
+        
+        START_DATE = args.start_date
+        END_DATE = args.end_date
+        FIRST_REVIEW_DATE = args.first_review_date
+        REVIEW_FREQ = int(args.review_freq)
+        RENT = int(args.rent)
 
-            print(START_DATE, END_DATE, FIRST_REVIEW_DATE, REVIEW_FREQ ,RENT)
-            exit(0)
-        except ValueError:
-            print("ERROR: Unsupported arguments")
-            exit(10)
+    except ValueError:
+        print("ERROR: Unsupported arguments")
+        exit(10)
 
+    review_dates_list = calculate_rent_review_dates(FIRST_REVIEW_DATE, END_DATE, REVIEW_FREQ )
 
+    rent_increase_dict = calculate_increasing_rent(RENT, review_dates_list)
+    print(rent_increase_dict)
+        
 
 if __name__ == "__main__":
     main()
