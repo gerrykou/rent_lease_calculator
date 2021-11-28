9a41881f6f6bb4ce0f97e2729614ae8ce66ce339
gerrykou <gerasimoskounadis@gmail.com>
2021-11-28 19:23:13 +0000

    reafactor

--- social_media_data_parser.py
+++ social_media_data_parser.py
@@ -1,3 +1,17 @@
+# 1/ Can you write a python script, a self-contained .py file to pass to your teammates, to
+# (a) parse the json script (below) from the scraper
+# (b) store the number of (i) posts and (ii)comments on a daily basis across the time period in a python
+# class
+# (c) calculate the sum of posts and comments on a daily basis
+# (d) calculate the aggregate number of posts and comments on a monthly basis
+# (e) store the monthly totals for the whole period for (i) posts and (ii) comments in a csv file
+# { “periodStart”: “15/02/11”,
+# “periodEnd”: “34/08/21”,
+# “monthlyPostingDay”: 11,
+# “comments” : [ [“2/3/21”, “Justin Bieber”, 5], [“5/4/21”, “Lady Gaga”, 6], ], [“5/4/21”, “Snoop Dog”,
+# 2] , [“13/5/21”,”Justin Bieber", 3]]
+# }
+
 # json_data = { "periodStart": "15/02/11",
 # "periodEnd": "34/08/21",
 # "monthlyPostingDay": 11,
@@ -22,116 +36,87 @@ json_string = """{
     "periodStart" : "15/02/11",
     "periodEnd" : "31/08/21",
     "monthlyPostingDay" : 11,
-    "comments" :[["2/3/21","Justin Bieber",5], ["5/4/21", "Lady Gaga", 6],["5/4/21", "Snoop Dog", 2], ["12/4/21", "Snoop Dog", 2], ["13/5/21","Justin Bieber", 3]]
+    "comments" :[["2/3/10","Justin Bieber",5],["2/3/21","Justin Bieber",5], ["5/4/21", "Lady Gaga", 6],["5/4/21", "Snoop Dog", 2], ["12/4/21", "Snoop Dog", 2], ["13/5/21","Justin Bieber", 3]]
 }"""
 
 
-def parse_data():
-    "loads data from json string, output is dictionary"
-    output = json.loads(json_string)
-    return output
-
-class Daily_sum:
+class Social_media_data:
     def __init__(self,
-                post_no,
-                comment_no,
-                date_day
-                ):
-
-        self.post_no = post_no
-        self.comment_no = comment_no
-        self.date_day = date_day
+                input_data):
+        self._data = json.loads(input_data)
+        self.period_start_date_obj = self._date_str2obj(self._data["periodStart"], INPUT_FORMAT)
+        self.period_end_date_obj = self._date_str2obj(self._data["periodEnd"], INPUT_FORMAT)
+        self.posts = self._data["comments"]
+        self.posts_dates_set = self._posts_dates_set(self.posts, INPUT_FORMAT)
+        self.intersection_dates_set = self._intersection_dates_set(self.period_start_date_obj, self.period_end_date_obj, self.posts_dates_set)
+        self.daily_stats_dict = self.calculate_stats(self.posts, self.intersection_dates_set, INPUT_FORMAT, OUTPUT_FORMAT)
+        self.monthly_stats_dict = self.calculate_stats(self.posts, self.intersection_dates_set, INPUT_FORMAT, YM_OUTPUT_FORMAT)
     
-    def add_post(self):
-        self.post_no += 1
 
-    def add_comment(self, comment_number):
-        self.comment_no += comment_number
+    def _posts_dates_set(self, posts, input_format):
+        posts_dates_set = set()
+        for post in posts: 
+            posts_dates_set.add(self._date_str2obj(post[0], input_format))
+        return posts_dates_set
+    
+    def _intersection_dates_set(self, period_start_date_obj, period_end_date_obj, posts_dates_set):
+        intersection_dates_set ={i for i in self._iter_dates(period_start_date_obj, period_end_date_obj) if i in posts_dates_set} #dates list for the period
+        print(intersection_dates_set)
+        return intersection_dates_set
 
+    def _iter_dates(self, start_date_obj, end_date_obj):
+        next_date = start_date_obj
+        while next_date < end_date_obj :
+            new_date_obj=(next_date + datetime.timedelta(days=1))
+            next_date = new_date_obj
+            yield next_date
 
-def _iter_dates(start_date_obj, end_date_obj):
-    next_date = start_date_obj
-    while next_date < end_date_obj :
-        new_date_obj=(next_date + datetime.timedelta(days=1))
-        next_date = new_date_obj
-        yield next_date
+    def _date_str2obj(self, date_string, format):
+        try:
+            date_obj = datetime.datetime.strptime(date_string, format)
+        except:
+            raise ValueError(f"Date {date_string} out of range")
+        return date_obj.date()
 
-def _date_str2obj(date_string, format):
-    try:
-        date_obj = datetime.datetime.strptime(date_string, format)
-    except:
-        raise ValueError(f"Date {date_string} out of range")
-    return date_obj.date()
+    def _datetime_obj2string(self, input_date, format):
+        date_obj=datetime.datetime.strftime(input_date, format)
+        return date_obj
 
-def _datetime_obj2string(input_date, format):
-    date_obj=datetime.datetime.strftime(input_date, format)
-    return date_obj
-
-def create_set_from_data_dates(data_list: list):
-    'input is list of strings, creates set of datetime.date objects'
-    posts_dates_set = set()
-    for date in data_list: 
-        posts_dates_set.add(_date_str2obj(date[0], INPUT_FORMAT))
-    return posts_dates_set
-
-
-def store_data_in_class_objects(data , dates_list):
-    posts_obj_dict = {}
-    for post in data["comments"]:
-        post_date_obj = _date_str2obj(post[0], INPUT_FORMAT)
-        if post_date_obj in dates_list:
-            comment_no = post[2]
-            if post_date_obj in posts_obj_dict.keys() :
-                posts_obj_dict[post_date_obj].add_comment(comment_no)
-                posts_obj_dict[post_date_obj].add_post()
-            else:
-                posts_obj_dict[post_date_obj] = Daily_sum(1, comment_no, post_date_obj)
-    return posts_obj_dict
-
-
-def monthly_and_daily_sum(posts_obj_dict):
-    monthly_sum_dict ={}
-    for i in posts_obj_dict.values():
-        sum_of_posts_daily = i.post_no
-        sum_of_comments_daily = i.comment_no
-        print(f'Daily sum for {i.date_day} is, posts: {sum_of_posts_daily} , comments: {sum_of_comments_daily}')
-        month_year_str = _datetime_obj2string(i.date_day, YM_OUTPUT_FORMAT)
-        if month_year_str in monthly_sum_dict.keys():
-            monthly_sum_dict[month_year_str]['monthly_post_sum'] += sum_of_posts_daily
-            monthly_sum_dict[month_year_str]['monthly_comments_sum'] += sum_of_comments_daily
-        else:
-            monthly_sum_dict[month_year_str] = {'monthly_post_sum' : sum_of_posts_daily, 'monthly_comments_sum' : sum_of_comments_daily }
-    print(monthly_sum_dict)
-    return monthly_sum_dict
+    def calculate_stats(self, posts, intersection_dates_set , input_format, output_format):
+        sum_dict = {}
+        for post in posts:
+            date_obj = self._date_str2obj(post[0], input_format)
+            if date_obj in intersection_dates_set:
+                date_str = self._datetime_obj2string(date_obj, output_format)
+                print(date_str)
+                comments_no = post[2]
+                if date_str in sum_dict.keys():
+                    sum_dict[date_str]['posts_sum'] += 1
+                    sum_dict[date_str]['comments_sum'] += comments_no
+                else:
+                    sum_dict[date_str] = {'posts_sum' : 1, 'comments_sum' : comments_no }
+        print(sum_dict)
+        return sum_dict
 
 
-def export_to_csv(filename, data):
-    with open(filename , mode="w") as csv_file:
-        fieldnames = ["date_month", "posts_sum", "comments_sum"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        for date, sum in data.items():
-            writer.writerow({ "date_month" : date , "posts_sum": sum["monthly_post_sum"], "comments_sum": sum["monthly_comments_sum"]})
-    return None
+    def export_to_csv(self,filename, data):
+        with open(filename , mode="w") as csv_file:
+            fieldnames = ["date_month", "posts_sum", "comments_sum"]
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            for date, sum in data.items():
+                writer.writerow({ "date_month" : date , "posts_sum": sum["posts_sum"], "comments_sum": sum["comments_sum"]})
+        return None
 
 
 def main():
-    data = parse_data()
+    data = Social_media_data(json_string)
 
-    start_date_obj = _date_str2obj(data["periodStart"],  INPUT_FORMAT)
-    end_date_obj = _date_str2obj(data["periodEnd"], INPUT_FORMAT)
+    start_date_str = data._datetime_obj2string(data.period_start_date_obj, OUTPUT_FORMAT)
+    end_date_str = data._datetime_obj2string(data.period_end_date_obj, OUTPUT_FORMAT)
+    filename = '_'.join([start_date_str, end_date_str, FILENAME])
 
-    print(f"Start date: {start_date_obj}, End_date: {end_date_obj}")
-
-    posts_dates_set = create_set_from_data_dates(data["comments"])  #set of post dates 
-
-    dates_list =[i for i in _iter_dates(start_date_obj, end_date_obj) if i in posts_dates_set] #dates list for the period
-
-    posts_obj_dict = store_data_in_class_objects(data , dates_list)
-
-    monthly_sum_dict = monthly_and_daily_sum(posts_obj_dict)
-
-    export_to_csv(FILENAME, monthly_sum_dict)
+    data.export_to_csv(filename, data.monthly_stats_dict)
 
 
 if __name__ == "__main__":
